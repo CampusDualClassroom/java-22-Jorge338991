59bcdc192e617033ae7e10ad76ccd8de00fae71c
Jorge338991 <jorge338991@gmail.com>
2025-01-11 21:40:07 +0100

    Se añade solución a Exercise22

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,20 +7,39 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
+        List<String> lista = new ArrayList();
+        String coleccion[] = {
+                "GKFFD",
+                "TNANA",
+                "MPMSL",
+                "PSWME",
+                "LZMLF",
+                "JYEBV",
+                "YELNT",
+                "JSNKR",
+                "JFESF",
+                "TMJLL"
+        };
 
+        for (String elemento : coleccion) {
+            lista.add(elemento);
+        }
+        return lista;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (String elemento : customList) {
+            System.out.println(elemento + " " + customList.indexOf(elemento));
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
-
+        List<String> myArrayList = createArrayList();
+        addElementToList(myArrayList, "AAAAA");
+        printElementsAndIndex(myArrayList);
     }
-
-
 }
