38150a3d25962fe1f99ab4ab6166443a8284f925
Jorge338991 <jorge338991@gmail.com>
2025-01-30 22:58:07 +0100

    Hot fix

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -2,28 +2,29 @@ package com.campusdual.classroom;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class Exercise22 {
 
     public static List<String> createArrayList() {
         List<String> lista = new ArrayList();
 
-                lista.add("GKFFD");
-                lista.add("TNANA");
-                lista.add("MPMSL");
-                lista.add("PSWME");
-                lista.add("LZMLF");
-                lista.add("JYEBV");
-                lista.add("YELNT");
-                lista.add("JSNKR");
-                lista.add("JFESF");
+        lista.add("GKFFD");
+        lista.add("TNANA");
+        lista.add("MPMSL");
+        lista.add("PSWME");
+        lista.add("LZMLF");
+        lista.add("JYEBV");
+        lista.add("YELNT");
+        lista.add("JSNKR");
+        lista.add("JFESF");
+        lista.add("TMJLL");
+
         return lista;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
         for (String elemento : customList) {
-            System.out.println(elemento + " " + customList.indexOf(elemento));
+            System.out.println(elemento.toString() + " " + customList.indexOf(elemento));
         }
     }
 
